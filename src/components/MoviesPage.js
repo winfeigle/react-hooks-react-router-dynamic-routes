b2f21b5b6d55439e6786c13d482fb8a49dc6028a
Win Feigle <win.feigle@gmail.com>
2022-07-29 19:16:08 -0600

    commit tree 5d8dbc54e46ea156ec4df703a3fdebf847ad5350

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,12 +1,21 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useRouteMatch } from "react-router-dom";
 import MoviesList from "./MoviesList";
+import MovieShow from "./MovieShow";
 
 function MoviesPage({ movies }) {
+  // useRouteMatch returns a special object with information about the currently matched route
+  const match = useRouteMatch();
+
   return (
     <div>
       <MoviesList movies={movies} />
+      <Route path={`${match.url}/:movieId`}>
+        {/* adding the movies object as a prop to MovieShow */}
+        <MovieShow movies={movies} />
+      </Route>
     </div>
   );
+
 }
 export default MoviesPage;
